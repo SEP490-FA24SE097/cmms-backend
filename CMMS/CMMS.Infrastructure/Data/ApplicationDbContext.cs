73902e1c11efb075f29c3893d0223ad03b68d9bd
noir-reg <hungphamphu159@gmail.com>
2024-10-24 01:17:19 +0700

    Add table for GoodsDeliveryNote

--- CMMS/CMMS.Infrastructure/Data/ApplicationDbContext.cs
+++ CMMS/CMMS.Infrastructure/Data/ApplicationDbContext.cs
@@ -8,25 +8,27 @@ namespace CMMS.Infrastructure.Data
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        public DbSet<Permission> Permissions { get;set; }
-        public DbSet<RolePermission> RolePermissions { get;set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<RolePermission> RolePermissions { get; set; }
 
-        public DbSet<Material>  Materials { get;set; }
-        public DbSet<Unit> Units  { get;set; }
-        public DbSet<Category> Categories  { get;set; }
-        public DbSet<Supplier> Suppliers { get;set; }
-        public DbSet<Store> Stores { get;set; }
-        
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<Unit> Units { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Store> Stores { get; set; }
 
-        public DbSet<UserPermission> UserPermissions { get;set; }
 
-        public DbSet<Brand> Brands { get;set; }
+        public DbSet<UserPermission> UserPermissions { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
         public DbSet<Attribute> Attributes { get; set; }
-        public DbSet<Variant> Variants { get;set; }
-        public DbSet<MaterialVariantAttribute> MaterialVariantAttributes { get;set; }
+        public DbSet<Variant> Variants { get; set; }
+        public DbSet<MaterialVariantAttribute> MaterialVariantAttributes { get; set; }
         public DbSet<Import> Imports { get; set; }
         public DbSet<Warehouse> Warehouses { get; set; }
         public DbSet<StoreInventory> StoreInventories { get; set; }
+        public DbSet<GoodsDeliveryNote> GoodsDeliveryNotes { get; set; }
+        public DbSet<GoodsDeliveryNoteDetail> GoodsDeliveryNoteDetails { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
